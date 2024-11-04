a41d730b35c9a52af3b73b0423ea2a8ed2d31605
Shihab066 <md.shihab066@gmail.com>
2024-11-04 22:10:22 +0600

    Add new API methods to get, remove, and retrieve wishlist courses

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -1,14 +1,80 @@
-import { getWishlistCollection } from "../collections.js";
+import { ObjectId } from "mongodb";
+import { getCoursesCollection, getWishlistCollection } from "../collections.js";
+
+export const getWishListItems = async (req, res) => {
+    try {
+        const wishlistCollection = await getWishlistCollection();
+        const { userId } = req.params;
+        const cursor = wishlistCollection.find({ userId }, { projection: { courseId: 1 } })
+        const wishlist = await cursor.toArray();
+        res.json(wishlist);        
+
+    } catch (error) {
+        console.log('Error fetching wishListItem:', error);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+};
 
 export const addWishListItem = async (req, res) => {
     try {
         const wishlistCollection = await getWishlistCollection();
         const wishlistItem = req.body;
+
+        const itemExist = await wishlistCollection.findOne(wishlistItem);
+        if (itemExist) {
+            return res.status(409).json({ error: true });
+        }
+
         const result = await wishlistCollection.insertOne(wishlistItem);
         res.status(201).json(result);
-        
+
     } catch (error) {
         console.log('Error adding wishListItem:', error);
-        res.status(500).json({ message: "Internal server error", error: error.message });        
+        res.status(500).json({ message: "Internal server error", error: error.message });
     }
-}
+};
+
+export const removeWishListItem = async (req, res) => {
+    try {
+        const wishlistCollection = await getWishlistCollection();
+        const { userId, courseId } = req.params;
+        const result = await wishlistCollection.deleteOne({ userId, courseId });
+        res.json(result);
+    } catch (error) {
+        console.log('Error deleting wishListItem:', error);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+};
+
+export const getWishlistCourses = async (req, res) => {
+    try {
+        const { wishlist} = req.body;       
+        
+        // Extract the courseIds from the wishlist items
+        const courseIds = wishlist.map(item => new ObjectId(item.courseId));
+
+        // Find all courses with courseIds from the wishlist
+        const courseCollection = await getCoursesCollection();
+        const options = {
+            projection: {
+                instructorName: 1,
+                courseName: 1,
+                courseThumbnail: 1,
+                level: 1,
+                rating: 1,
+                totalReviews: 1,
+                totalModules: 1,
+                price: 1,
+                discount: 1
+            }
+        }
+        const courses = await courseCollection.find({ _id: { $in: courseIds } }, options).toArray();
+
+        // Respond with the course details
+        res.json(courses);        
+        
+    } catch (error) {
+        console.error("Error fetching wishlist courses:", error);
+        res.status(500).json({ message: "An error occurred", error });
+    }
+};
--- routes/wishlistRouter.js
+++ routes/wishlistRouter.js
@@ -1,8 +1,19 @@
 import express from 'express';
-import { addWishListItem } from '../controllers/wishlistController.js';
+import { addWishListItem, getWishlistCourses, getWishListItems, removeWishListItem } from '../controllers/wishlistController.js';
 
 const wishlistRouter = express.Router();
 
+// get wishlist by userId
+wishlistRouter.get('/get/:userId', getWishListItems);
+
+// add wishlist item
 wishlistRouter.post('/add', addWishListItem);
+
+// delete wishlist item by userId and courseId
+wishlistRouter.delete('/delete/:userId/:courseId', removeWishListItem);
+
+// get wishlist courses by wishlistItems
+wishlistRouter.post('/courses', getWishlistCourses);
+
 
 export default wishlistRouter;
