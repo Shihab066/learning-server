25057dbde6d960a0a496bbec5587249e92c3c30a
Shihab066 <md.shihab066@gmail.com>
2025-01-25 23:37:30 +0600

    Enhance wishlist router security: - Added middleware for token verification, active user validation, and student role verification to secure the wishlist API.

--- routes/wishlistRouter.js
+++ routes/wishlistRouter.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { addWishListItem, getWishlistCourses, getWishListItems, removeWishListItem } from '../controllers/wishlistController.js';
+import { verifyToken } from '../controllers/jwtController.js';
+import { verifyActiveUser, verifyStudent } from '../controllers/authorizationController.js';
 
 const wishlistRouter = express.Router();
+wishlistRouter.use(verifyToken, verifyActiveUser, verifyStudent);
 
 // get wishlist by userId
 wishlistRouter.get('/get/:userId', getWishListItems);
@@ -14,7 +17,5 @@ wishlistRouter.post('/add', addWishListItem);
 
 // delete wishlist item by userId and courseId
 wishlistRouter.delete('/delete/:userId/:courseId', removeWishListItem);
-
-
 
 export default wishlistRouter;
